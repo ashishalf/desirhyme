0ab295463cfeab0e3e1dbab57a345a5bc61ce5f6
Ashish Kumar <87820654+ashishalf@users.noreply.github.com>
2024-02-06 17:39:56 +0530

    Update Footer.jsx
    Rename

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
   return (
     <>
     <hr />
-    <p style={{textAlign: 'center', fontSize: isMobile ? '10px' : '15px'}}>© hiphop 2023 All Rights Reserved.</p>
+    <p style={{textAlign: 'center', fontSize: isMobile ? '10px' : '15px'}}>© desirhyme 2024 All Rights Reserved.</p>
     </>
   )
 }
