86577ee3ae1cb2d80dfc709c0676e2de564d4b40
Ashish Kumar <87820654+ashishalf@users.noreply.github.com>
2024-09-12 14:09:53 +0530

    Update ObjectRenderer.jsx

--- src/component/ObjectRenderer.jsx
+++ src/component/ObjectRenderer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { databases } from "../appwrite/appwrite"; // Import Appwrite database instance
 import spotify from "../assets/spotify.png";
 import { Link } from "react-router-dom";
+import { Query } from 'appwrite'; // Import Query
 
 function ObjectRenderer() {
   const [items, setItems] = useState([]);
@@ -9,6 +10,7 @@ function ObjectRenderer() {
   const collectionId = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
 
   useEffect(() => {
+    // Fetch data from Appwrite
     const fetchData = async () => {
       try {
         const response = await databases.listDocuments(databaseId, collectionId);
@@ -33,18 +35,28 @@ function ObjectRenderer() {
         <div
           key={index}
           style={{
-            margin: "20px 10px",
+            margin: "50px 100px",
             background: "black",
             borderRadius: "10px",
             display: "flex",
-            flexDirection: "column",
+            justifyContent: "space-between",
+            alignItems: "center",
             padding: "20px",
+            flexWrap: "wrap",
           }}
         >
-          <div>
+          <div
+            style={{
+              flex: "1",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              width: "50%", // Adjust the width as needed
+            }}
+          >
             <img
               style={{
-                width: "100%",
+                width: "60%",
                 borderRadius: "10px",
                 objectFit: "cover",
               }}
@@ -54,27 +66,32 @@ function ObjectRenderer() {
             />
           </div>
           <div
-            style={{
-              textAlign: "center",
-              margin: "20px 0",
-            }}
+            style={{ flex: "1", textAlign: "justify", margin: "0 80px 0 0" }}
           >
-            <h1 className="title" style={{ fontSize: "24px", color: "white" }}>
+            <h1 className="title" style={{ fontSize: "36px", color: "white" }}>
               {item.title}
             </h1>
             <p
               className="details"
               style={{
-                fontSize: "16px",
+                fontSize: "24px",
                 color: "white",
+                margin: "30px 0",
                 fontFamily: "Montserrat",
               }}
             >
               {item.details}
             </p>
-            <button style={{ borderRadius: "50px", padding: "12px 20px", background:'#1ED760' }}>
+            <button
+              style={{
+                borderRadius: "50px",
+                padding: "16px 30px",
+                marginBottom: "20px",
+                background: '#1ED760',
+              }}
+            >
               <Link className="link" to={item.link}>
-                <img style={{ width: "80px" }} src={spotify} alt="Spotify" />
+                <img style={{ width: "120px" }} src={spotify} alt="Spotify" />
               </Link>
             </button>
           </div>
